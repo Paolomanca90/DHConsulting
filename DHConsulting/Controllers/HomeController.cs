e11001f313ee1fb2ae51b7d7ab3cdc92c4a5a941
Paolo Leucio Manca <paolomanca1990@gmail.com>
2023-11-08 00:37:25 +0000

    profile style

--- DHConsulting/Controllers/HomeController.cs
+++ DHConsulting/Controllers/HomeController.cs
@@ -2,10 +2,14 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.Entity;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DHConsulting.Controllers
 {
@@ -104,7 +108,7 @@ namespace DHConsulting.Controllers
                     {
                         //rimuovo completamente il prodotto dal carrello se la quantità è 1
                         carrello.Remove(d);
-                        if(carrello.Count > 0)
+                        if (carrello.Count > 0)
                         {
                             SaveCart(carrello);
                         }
@@ -218,6 +222,72 @@ namespace DHConsulting.Controllers
             }
             ViewBag.Errore = "Errore durante la procedura";
             return View();
+        }
+
+        //Metodo per creare la mail di richiesta per il pacchetto professional
+        public void MailProPack(string description)
+        {
+            var utente = db.Cliente.FirstOrDefault(x => x.Username == User.Identity.Name);
+            string senderEmail = ConfigurationManager.AppSettings["SmtpSenderEmail"];
+            string senderPassword = ConfigurationManager.AppSettings["SmtpSenderPassword"];
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(senderEmail, senderPassword),
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage()
+            {
+                From = new MailAddress(senderEmail, "Paolo Manca Consulting"),
+                Subject = "Richiesta Pacchetto Pro",
+                Body = @"
+                        <section class=""max-w-2xl px-6 py-8 mx-auto bg-white dark:bg-gray-900"">
+                            <main class=""mt-8"">
+                                <h2 class=""text-gray-700 dark:text-gray-200"">Pacchetto pro</h2>
+
+                                <p class=""mt-2 leading-loose text-gray-600 dark:text-gray-300"">
+                                    Hai appena ricevuto una richiesta per il pacchetto pro con il seguente testo:
+                                </p>
+
+                                <p class=""mt-2 leading-loose text-gray-600 dark:text-gray-300 italic"">
+                                    " + description + @"
+                                </p>
+                            </main>
+                            <footer class=""mt-8"">
+                                <p class=""text-gray-500 dark:text-gray-400"">
+                                    Questa email è stata inviata da " + utente.Nome + " " + utente.Cognome + @"
+                                </p>
+                                <p class=""mt-1 leading-loose text-gray-600 dark:text-gray-300"">
+                                    Email: <a href="" + utente.Email + "" class=""text-blue-600 hover:underline dark:text-blue-400"" target=""_blank"">" + utente.Email + @"</a>.
+                                </p>
+                                <p class=""mt-1 leading-loose text-gray-600 dark:text-gray-300"">
+                                    Telefono: " + utente.Phone + @"
+                                </p>
+                                <p class=""mt-3 text-gray-500 dark:text-gray-400"">" + DateTime.Now + @"</p>
+                            </footer>
+                        </section>",
+                IsBodyHtml = true, 
+            };
+            mailMessage.To.Add(senderEmail);
+
+            smtpClient.Send(mailMessage);
+        }
+
+        //Metodo per inviare la mail di richiesta del pro pack
+        [HttpPost]
+        public ActionResult ProfessionalPack(string description)
+        {
+            int id = Convert.ToInt16(TempData["IdProdotto"]);
+            if(description != "")
+            {
+                MailProPack(description);
+                TempData["Successo"] = "Richiesta inviata con successo. Entro 48h verrai ricontattato per approfondimenti sul tuo progetto";
+                return RedirectToAction("Details", "Home", new { id = id });
+            }
+            TempData["Errore"] = "Il campo descrizione non può essere vuoto";
+            return RedirectToAction("Details", "Home", new { id = id });
         }
     }
 }
